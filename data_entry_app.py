542fcca97245e41c29dfe2a72c1c904270595b4e
Cornel-hub38 <33731777+Cornel-hub38@users.noreply.github.com>
2025-09-08 13:18:38 +0100

    More addition of code to  data_entry_app.py and class LabelInput()

--- data_entry_app.py
+++ data_entry_app.py
@@ -1,10 +1,133 @@
 # data _entry_app.py   Page 136
 
-from tkinter import tk   # Saturday 6th September 2025 @1014 on starting on page 100
+import tkinter as tk  # Saturday 6th September 2025 @1014 on starting on page 100
 from tkinter import ttk
 from datetime import datetime
 from pathlib import Path
 import csv
+
+from pywin.framework.dlgappcore import AppDialog
+
+
+variables = dict()    # Sunday 7th September 2025 @1540     Page 65
+records_saved = 0
+
+root = tk.Tk()
+root.title('ABQ Data Entry Application')
+root.columnconfigure(0, weight=1)
+
+ttk.Label(    #  Page 66
+    root, text="ABQ Data Entry Application",
+    font=("TkDefaultFont", 16)
+    ).grid()
+
+
+drf = ttk.Frame(root)      #  Page 66
+drf.grid(padx=10, sticky=(tk.E + tk.W))
+drf.columnconfigure(0, weight=1)
+
+r_info = ttk.LabelFrame(drf, text='Record Information')   #   Page 67
+r_info.grid(sticky=(tk.W + tk.E))
+for i in range(3):
+    r_info.columnconfigure(i, weight=1)
+
+
+variables['Date'] = tk.StringVar()
+ttk.Label(r_info, text='Date').grid(row=0, column=0)
+ttk.Entry(
+    r_info, textvariable=variables['Date']
+).grid(row=1, column=0, sticky=(tk.W + tk.E))
+
+
+
+time_values = ['8:00', '12:00', '16:00', '20:00']
+variables['Time'] = tk.StringVar()
+ttk.Label(r_info, text='Time').grid(row=0, column=1)
+ttk.Combobox(
+    r_info, textvariable=variables['Time'], values=time_values
+).grid(row=1, column=1, sticky=(tk.W + tk.E))              #Page 68
+
+variables['Technician'] = tk.StringVar()        # Sunday 7th September 2025 @1606     Page 68
+ttk.Label(r_info, text='Technician').grid(row=0, column=2)      #  Monday September 8th 2025 @1115   Page 68
+ttk.Entry(
+    r_info, textvariable=variables['Technician']
+).grid(row=1, column=2, sticky=(tk.W + tk.E))
+
+variables['Lab'] = tk.StringVar()
+ttk.Label(r_info, text='Lab').grid(row=2, column=0)
+labframe = ttk.Frame(r_info)
+for lab in ('A', 'B', 'C'):
+    ttk.Radiobutton(
+        labframe, value=lab, text=lab, variable=variables['Lab']
+    ).pack(side=tk.LEFT, expand=True)
+    labframe.grid(row=3, column=0, sticky=(tk.W + tk.E))
+
+variables['Plot'] = tk.IntVar()
+ttk.Label(r_info, text='Plot').grid(row=2, column=1)
+ttk.Combobox(
+    r_info,
+    textvariable=variables['Plot'],
+    values=list(range(1, 21)
+).grid(row=3, column=1, sticky=(tk.W + tk.E))
+
+variables['Seed Sample'] = tk.StringVar()
+ttk.Label(r_info, text='Seed Sample').grid(row=2, column=2)         #   Page 69
+ttk.Entry(
+    r_info,
+    textvariable=variables['Seed Sample']
+).grid(row=3, column=2, sticky=(tk.W + tk.E))
+
+
+# The Envirnment Data ectin    Page 69
+e_info = ttk.LabelFrame(drf, text='Environment Data')
+e_info.grid(sticky=(tk.W + tk.E))
+for i in range(3):
+    e_info.columnconfigure(i, weight=1)
+
+variables['Humidity'] = tk.DoubleVar()
+ttk.Label(e_info, text="Humidity (g/m3)").grid(row=0, column=0)
+ttk.Spinbox(
+    e_info, textvariable=variables['Humidity'],
+    from_=0.5, to=52.0, increment=0.01,
+).grid(row=1, column=0, sticky=(tk.W + tk.E))
+
+
+variables['Light'] = tk.DoubleVar()
+ttk.Label(e_info, text='Light (klx)').grid(row=0, column=1)
+ttk.Spinbox(
+    e_info, textvariable=variables['Light'],
+    from_=0, to=100, increment=0.01
+).grid(row=1, column=1, sticky=(tk.W + tk.E))
+
+variables["Temperature"] = tk.DoubleVar()
+ttk.Label(e_info, text="Temperature (oC)").grid(row=0, column=2)
+ttk.Spinbox(
+    e_info, textvariable=variables["Temperature"],
+    from_=4, to=40, increment=.01
+).grid(row=1, column=2, sticky=(tk.W + tk.E))     #  end of Page 69
+
+
+variables['Equipment Fault'] = tk.BooleanVar(value=False)         #   Page 70
+ttk.Checkbutton(
+    e_info, variable=variables['Equipment Fault'],
+    text='Equipment Fault'
+).grid(row=2, column=0, sticky=tk.W, pady=5)
+
+p_info = ttk.LabelFrame(drf, text="Plant Data")
+p_info.grid(sticky=(tk.W + tk.E))
+for i in range(3):
+    p_info.columnconfigure(i, weight=1)
+
+variables['Plants'] = tk.IntVar()
+ttk.Label(p_info, text="Plants").grid(row=0, column=0)
+
+
+
+
+
+
+
+
 
 
 class BoundText(tk.Text):
@@ -186,7 +309,15 @@ class DataRecordForm(ttk.Frame):         # Creating a form class   Page 106
         ).grid(sticky=tk.W, row=3, column=0)
 
         #  Now its time for the buttons
-        buttons =
+        buttons = tk.Frame(self)
+        buttons.grid(sticky=tk.W + tk.E, row=4)
+        self.savebutton = ttk.Button(
+            buttons, text="Save", command=self.master._on_save)
+        self.savebutton.pack(side=tk.RIGHT)
+
+        self.resetbutton = ttk.Button(
+            buttons, text="Reset", command=self.reset)
+        self.resetbutton.pack(side=tk.RIGHT)    #    end f page 110
 
 
 
@@ -484,8 +615,9 @@ class ValidatedRadioGroup(ttk.Frame):
 
 
 
-
-
+if __name__ == "__main__":   #  Sunday 7th September 2025 @1024    Page 114
+    app = LabelInput()
+    app.mainloop()
 
 
 
