5d3ea3db4d93c2478410c0c41dc1682a42e28c64
Cornel-hub38 <33731777+Cornel-hub38@users.noreply.github.com>
2025-09-12 09:51:54 +0100

    start chapter 4, Organising our code with classes, data_entry_app.py and tkinter_classes_demo.py,   first piece of code

--- tkinter_classes_demo.py
+++ tkinter_classes_demo.py
@@ -2,3 +2,28 @@ import tkinter as tk   #  Friday September 12 2025 @0926     page 92
 import json
 
 
+class JSONVar(tk.StringVar):
+    """A Tk variable that can hold dicts and lists"""
+    def __init__(self, *args, **kwargs):
+        kwargs['value'] = json.dumps(kwargs.get('value'))
+        super().__init__(*args, **kwargs)
+
+
+    def set(self, value, *args, **kwargs):
+        string = json.dumps(value)
+        super().set(string, *args, **kwargs)
+
+    def get(self, *args, **kwargs):      #Page 93
+        string = super().get(*args, **kwargs)
+        return json.loads(string)
+
+
+root = tk.Tk()
+var1 = JSONVar(root)
+var1.set([1, 2,3])
+var2  = JSONVar(root, value={'a': 10, 'b': 15})
+
+print("Var1: ", var1.get()[1])    # should print 2
+
+print("Var2: ", var2.get()['b'])   # should print 15
+
